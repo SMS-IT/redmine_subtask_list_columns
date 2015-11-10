3171de2d2fc7cf37a2c9302bf713f7466bdbf69f
E.Redkozubov <eugengeredkozubov@gmail.com>
2015-11-10 15:39:57 +0400

    Fix bug: "As soon as you create a custom field for the parent task (list, text or any other kind) you get an error when trying to visit the parent task."

--- subtask_list_columns/lib/subtask_list_columns_lib.rb
+++ subtask_list_columns/lib/subtask_list_columns_lib.rb
@@ -22,17 +22,6 @@ module SubtaskListColumnsLib
         field_headers = ''
         map = []
         s = '<form><table class="list issues">'
-         
-        issue_list(issue.descendants.visible.sort_by(&:lft)) do |child|
-            custom_fields = child.available_custom_fields
-            custom_fields.each do |field|
-              if field.listed? and map.find_index(field.name).nil?
-                field_headers << content_tag('th', field.name)
-                map << field.name
-              end
-            end
-          end
-        
         
         if(fields_list.count == 0) 
           #if the project column is not set, show: subject, status, assigned_to and done_ratio
@@ -54,17 +43,6 @@ module SubtaskListColumnsLib
                content_tag('td align="center"', h(child.status)) +
                content_tag('td align="center"', link_to_user(child.assigned_to))
 
-            map.each do |field|
-              found = false
-              child.custom_field_values.each do |cv|
-                if (field == cv.custom_field.name)
-                  found = true
-                  field_content << content_tag('td align="right"', cv.value)
-                end
-              end
-              field_content << content_tag('td bgcolor="#dddddd" class="no-field-owner"', '') unless found
-            end
-      
             field_content << content_tag('td', progress_bar(child.done_ratio, :width => '80px'))
             field_values << content_tag('tr', field_content, :class => css).html_safe
           end
